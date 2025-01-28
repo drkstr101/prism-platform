9290657894098121125bee58db7f70eb978b41a8
Aaron R Miller <amiller@aps.org>
2025-01-27 23:52:17 -0500

    chore: fix baseURL

--- apps/screenplay/playwright.config.ts
+++ apps/screenplay/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4300';
 
 /**
  * Read environment variables from file.
